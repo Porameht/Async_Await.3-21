1f49de17ae503447ba7dfbe68a028fab1295d5ad
frank.sativa <porameht@outlook.com>
2022-03-27 07:30:43 +0700

    first commit

---
+++ start.js
@@ -0,0 +1,73 @@
+(() => {
+  // start code
+
+  // 1.How Asynchorous code works in JavsScript
+    function simulateAsyncAPI(text, timeout) {
+      setTimeout(() => console.log(text), timeout);
+    }
+    simulateAsyncAPI('A',1000)
+    simulateAsyncAPI('B',500)
+    simulateAsyncAPI('C',100)
+
+  //   // 2.Callback
+    function simulateAsyncAPI(text, timeout, Callback) {
+      setTimeout(() => {
+        console.log(text);
+
+        if (Callback) {
+          Callback();
+        }
+      }, timeout);
+    }
+  //   Callback hell
+    simulateAsyncAPI("A", 1000, () => {
+      simulateAsyncAPI("B", 1000, () => {
+        simulateAsyncAPI("C", 500);
+      });
+    });
+
+  // 3.Promise
+    function simulateAsyncAPI(text, timeout) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (text === "B") return reject("B got rejected");
+          console.log(text);
+          resolve();
+        }, timeout);
+      });
+    }
+
+    simulateAsyncAPI("A", 1000)
+      .then(() => {
+        return simulateAsyncAPI("B", 500);
+      })
+      .then(() => {
+        return simulateAsyncAPI("C", 100);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  // 4.Async/Await
+  function simulateAsyncAPI(text, timeout) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // if (text === "B") return reject("B got rejected");
+        console.log(text);
+        resolve();
+      }, timeout);
+    });
+  }
+
+  async function run() {
+    try {
+      await simulateAsyncAPI("A", 1000);
+      await simulateAsyncAPI("B", 500);
+      await simulateAsyncAPI("C", 100);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  run();
+})();
